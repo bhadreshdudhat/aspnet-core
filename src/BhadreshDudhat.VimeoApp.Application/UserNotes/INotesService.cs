5c5562ae5a0a716eca1e97eadead44fcac4490d5
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-17 02:44:34 -0500

    Create interface contract  for NoteService for CRUD on notes in Application project

---
+++ src/BhadreshDudhat.VimeoApp.Application/UserNotes/INotesService.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using BhadreshDudhat.VimeoApp.UserNotes.Dto;
+using System.Threading.Tasks;
+
+namespace BhadreshDudhat.VimeoApp.UserNotes
+{
+    public interface INotesService : IApplicationService
+    {
+
+        Task<ListResultDto<UserNoteDto>> GetAll(string useremail);
+        Task<ListResultDto<string>> GetAllNotes(string useremail);
+
+        Task CreateNote(UserNoteDto note);
+
+        void UpdateNote(int id, UserNoteDto note);
+        void DeleteNote(int id);
+
+    }
+}
