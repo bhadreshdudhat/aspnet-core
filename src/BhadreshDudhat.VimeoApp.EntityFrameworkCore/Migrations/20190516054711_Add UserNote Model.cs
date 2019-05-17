b752d9fb9911211e983839edfa93c91a826e98ae
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-17 02:30:19 -0500

    Add Migrations and update database

---
+++ src/BhadreshDudhat.VimeoApp.EntityFrameworkCore/Migrations/20190516054711_Add UserNote Model.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BhadreshDudhat.VimeoApp.Migrations
+{
+    public partial class AddUserNoteModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserNotes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserEmail = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(maxLength: 2000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserNotes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserNotes");
+        }
+    }
+}
---
+++ src/BhadreshDudhat.VimeoApp.EntityFrameworkCore/Migrations/20190516171326_Change UserNote Model.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BhadreshDudhat.VimeoApp.Migrations
+{
+    public partial class ChangeUserNoteModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserEmail",
+                table: "UserNotes",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "UserNotes",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 2000);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserEmail",
+                table: "UserNotes",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "UserNotes",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
