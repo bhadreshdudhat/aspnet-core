4c2681e80a88de516c2534f36b6596e0f55aa54f
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-17 02:41:07 -0500

    Add Dbset for UserNotes table in our application DbContext

--- src/BhadreshDudhat.VimeoApp.EntityFrameworkCore/EntityFrameworkCore/VimeoAppDbContext.cs
+++ src/BhadreshDudhat.VimeoApp.EntityFrameworkCore/EntityFrameworkCore/VimeoAppDbContext.cs
@@ -1,15 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Abp.Zero.EntityFrameworkCore;
+﻿using Abp.Zero.EntityFrameworkCore;
 using BhadreshDudhat.VimeoApp.Authorization.Roles;
 using BhadreshDudhat.VimeoApp.Authorization.Users;
+using BhadreshDudhat.VimeoApp.Domain.Models;
 using BhadreshDudhat.VimeoApp.MultiTenancy;
+using Microsoft.EntityFrameworkCore;
 
 namespace BhadreshDudhat.VimeoApp.EntityFrameworkCore
 {
     public class VimeoAppDbContext : AbpZeroDbContext<Tenant, Role, User, VimeoAppDbContext>
     {
         /* Define a DbSet for each entity of the application */
-        
+        public DbSet<UserNote> UserNotes { get; set; }
+
         public VimeoAppDbContext(DbContextOptions<VimeoAppDbContext> options)
             : base(options)
         {
