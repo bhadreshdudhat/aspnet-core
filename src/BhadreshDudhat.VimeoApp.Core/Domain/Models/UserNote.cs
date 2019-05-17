691f22891bc1c134d3acac29e4b623c060079fb8
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-17 02:24:23 -0500

    Add UserNote Model in Core project

---
+++ src/BhadreshDudhat.VimeoApp.Core/Domain/Models/UserNote.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace BhadreshDudhat.VimeoApp.Domain.Models
+{
+    public class UserNote : Entity
+    {
+        [Required]
+        public string UserEmail { get; set; }
+
+        [Required]
+        public string Notes { get; set; }
+    }
+}
+
+//ABP's base Entity class, which includes Id property as int by default.
