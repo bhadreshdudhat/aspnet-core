79e1d3daffe9054a27778e6d8900c2da89fc1a59
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-17 02:25:59 -0500

    Add DTO for UserNote Model with Automapping  in Application Layer project

---
+++ src/BhadreshDudhat.VimeoApp.Application/UserNotes/Dto/UserNoteDto.cs
@@ -0,0 +1,17 @@
+﻿using Abp.AutoMapper;
+using BhadreshDudhat.VimeoApp.Domain.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace BhadreshDudhat.VimeoApp.UserNotes.Dto
+{
+    [AutoMapFrom(typeof(UserNote))]
+    [AutoMapTo(typeof(UserNote))]
+    public class UserNoteDto
+    {
+        [Required]
+        public string UserEmail { get; set; }
+
+        [Required]
+        public string Notes { get; set; }
+    }
+}
